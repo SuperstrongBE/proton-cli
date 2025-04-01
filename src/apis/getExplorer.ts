796e5fcc1e92be2aafa011d536150a993ded1f0f
Remy Chauveau <rem.chauveau@gmail.com>
2025-04-01 17:12:02 +0200

    Generalize the usage of getExplorer in all command output

--- src/apis/getExplorer.ts
+++ src/apis/getExplorer.ts
@@ -4,9 +4,9 @@ export function getExplorer () {
 	const chain: string = config.get('currentChain')
 
     if (chain === 'proton') {
-        return 'https://protonscan.io'
+        return 'https://explorer.xprnetwork.org/'
     } else if (chain === 'proton-test') {
-        return 'https://testnet.protonscan.io'
+        return 'https://testnet.explorer.xprnetwork.org/'
     }
 
     throw new Error('Chain not supported')
