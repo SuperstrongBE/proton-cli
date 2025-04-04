ac67622c489b95977d1a7bfbe16738d8499e98ea
Remy Chauveau <rem.chauveau@gmail.com>
2025-04-04 10:09:12 +0200

    Fix trailing slash inf getExplorer()

--- src/apis/getExplorer.ts
+++ src/apis/getExplorer.ts
@@ -3,9 +3,9 @@ import { config } from "../storage/config"
 export function getExplorer () {
 	const chain: string = config.get('currentChain')
     if (chain === 'proton') {
-        return 'https://explorer.xprnetwork.org/'
+        return 'https://explorer.xprnetwork.org'
     } else if (chain === 'proton-test') {
-        return 'https://testnet.explorer.xprnetwork.org/'
+        return 'https://testnet.explorer.xprnetwork.org'
     }
 
     throw new Error('Chain not supported')
